4582a73ee3e4b5831a48b304ee5fbd2a24db3e99
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-08-06 05:28:58 +0400

    Added  a new logic.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WelcomeSummerSchool2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Title = "Welcome to Summer School 2021!";
+
+            Console.WriteLine("Today you will receive the first assignment!");
+
+            Console.ReadLine();
+        }
+    }
+}
